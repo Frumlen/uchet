2790f5ce294097dd68970149ca12107f873e5961
magomed <mr.fila@gmail.com>
2021-02-21 15:29:40 +0300

    models.CASCADE

--- item/models.py
+++ item/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=256, verbose_name="Название категории")
-    parent = models.ForeignKey("self", on_delete=models.DO_NOTHING, verbose_name="Родительская категория")
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, verbose_name="Родительская категория")
 
     def __str__(self):
         return self.name if not self.parent else "{} -> {}".format(self.parent.name, self.name)
@@ -22,7 +22,7 @@ class Region(models.Model):
 
 class ItemImage(models.Model):
     image = models.ImageField(upload_to='images/%Y/%m/%d', verbose_name="Изображение")
-    item = models.ForeignKey("item.Item", on_delete=models.DO_NOTHING, verbose_name="Связанный товар")
+    item = models.ForeignKey("item.Item", on_delete=models.CASCADE, verbose_name="Связанный товар")
 
     class Meta:
         verbose_name = 'Изображение товара'
@@ -34,7 +34,7 @@ class Item(models.Model):
     comment = models.TextField(null=True, blank=True,verbose_name="Комментарий")
     description = models.TextField(null=True, blank=True, verbose_name="Описание")
     material = models.TextField(null=True, blank=True, verbose_name="Материал")
-    region = models.ForeignKey(Region, null=True, blank=True, on_delete=models.DO_NOTHING, verbose_name="Регион")
+    region = models.ForeignKey(Region, null=True, blank=True, on_delete=models.CASCADE, verbose_name="Регион")
     price_buy = models.DecimalField(null=True, blank=True, max_digits=10000000, decimal_places=2, verbose_name="Цена закупочная")
     price_sell = models.DecimalField(null=True, blank=True, max_digits=10000000, decimal_places=2, verbose_name="Цена продажи")
     count = models.IntegerField(default=1, verbose_name="Количество")
